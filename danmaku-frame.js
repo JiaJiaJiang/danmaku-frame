44f53287d25e04e369a853c1eff92feefbedcd4f
luojia <luojia@luojia.me>
2017-02-01 01:06:15 +0800

    no message

--- danmaku-frame.js
+++ danmaku-frame.js
@@ -1,5 +1,6 @@
 /*
 Copyright luojia@luojia.me
+LGPL license
 */
 
 'use strict';
@@ -9,29 +10,30 @@ class DanmakuFrame{
 		if(canvas && (canvas instanceof HTMLCanvasElement === false))
 			throw('The first arg should be a canvas or empty');
 		this.canvas=canvas||document.createElement('canvas');//the canvas
-		this.context=this.canvas.getContext('2d');//the canvas context
+		this.context2d=this.canvas.getContext('2d');//the canvas context
 		this.COL=new CanvasObjLibrary(this.canvas);//the library
 
-		this.modules=new Map();//constructed module list
+		this.rate=1;
+		this.timeBase=0;
+		this.modules={};//constructed module list
 		for(let m of DanmakuFrame.moduleList){//install all modules
 			this.install(m[0]);
 		}
 	}
 	enable(name){
-		let module=this.modules.get(name);
-		if(module){
-			module.enabled=true;
-			module.enable();
-			return true;
-		}
-		return this.install(name);
+		let module=this.modules[name];
+		if(!module)return this.install(name);
+		module.enabled=true;
+		module.enable();
+		return true;
 	}
 	disable(name){
-		let module=this.modules.get(name);
+		let module=this.modules[name];
 		if(!module)return false;
 		module.enabled=false;
 		module.disable();
 		return true;
+	}
 	install(name){
 		let mod=DanmakuFrame.moduleList.get(name);
 		if(!mod)throw('Module ['+name+'] does not exist.');
@@ -39,20 +41,38 @@ class DanmakuFrame{
 		if(module instanceof DanmakuFrameModule === false)
 			throw('Constructor of '+name+' is not extended from DanmakuFrameModule');
 		module.enabled=true;
-		this.modules.set(name,module);
+		this.modules[name]=module;
 		return true;
 	}
-	danmaku(danmakuObj){
-		for(let m of this.modules)
-			m[1].danmaku(danmakuObj);
+	set time(t){//current media time (ms)
+		this.timeBase=Date.now()-t;
 	}
-	static add(name,module){
+	get time(){
+		return Date.now()-this.timeBase;
+	}
+	load(danmakuObj){
+		for(let m in this.modules)
+			this.modules[m].load(danmakuObj);
+	}
+	unload(danmakuObj){
+		for(let m in this.modules)
+			this.modules[m].unload(danmakuObj);
+	}
+	draw(){
+		
+	}
+	FPS(fps){
+		if(fps===0){
+
+		}
+	}
+	static addModule(name,module){
 		if(this.moduleList.has(name)){
 			console.warn('The module "'+name+'" has already been added.');
 			return;
 		}
 		this.moduleList.set(name,module);
-	}
+	} 
 }
 
 DanmakuFrame.moduleList=new Map();
@@ -62,7 +82,7 @@ class DanmakuFrameModule{
 		this.frame=frame;
 		this.enabled=false;
 	}
-	danmaku(){}
+	load(){}
 	enable(){}
 	disable(){}
 }
