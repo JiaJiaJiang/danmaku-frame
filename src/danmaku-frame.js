e5b367fa6d79a4b84d95dad075fa702670994ae5
luojia <luojia@luojia.me>
2017-03-06 00:27:10 +0800

    no message

--- src/danmaku-frame.js
+++ src/danmaku-frame.js
@@ -12,7 +12,7 @@ class DanmakuFrame{
 		this.rate=1;
 		this.timeBase=0;
 		this.media=null;
-		this.fps=30;
+		this.fps=0;
 		this.working=false;
 		this.modules={};//constructed module list
 		for(let m of DanmakuFrame.moduleList)//init all modules
@@ -23,7 +23,15 @@ class DanmakuFrame{
 			});
 			this.resize();
 		},0);
-		
+		const draw=()=>{
+			if(this.fps===0){
+				requestAnimationFrame(draw);
+			}else{
+				setTimeout(draw,1000/this.fps);
+			}
+			this.moduleFunction('draw');
+		}
+		draw();
 	}
 	enable(name){
 		let module=this.modules[name];
@@ -55,7 +63,7 @@ class DanmakuFrame{
 		this.moduleFunction('time',t);//let all mods know when the time be set
 	}
 	get time(){
-		return this.media?this.media.currentTime*1000000:(Date.now()-this.timeBase);
+		return this.media?(this.media.currentTime*1000)|0:(Date.now()-this.timeBase);
 	}
 	load(danmakuObj){
 		this.moduleFunction('load');
@@ -67,33 +75,23 @@ class DanmakuFrame{
 		this.moduleFunction('unload',danmakuObj);
 	}
 	start(){
+		if(this.working)return;
+		console.log('start')
 		this.working=true;
 		this.moduleFunction('start');
-		this.draw();
 	}
 	pause(){
+		console.log('pause')
 		this.working=false;
 		this.moduleFunction('pause');
 	}
-	stop(){
-		this.working=false;
-		this.moduleFunction('stop');
-	}
 	resize(){
+		console.log('resize')
 		this.moduleFunction('resize');
 	}
 	moduleFunction(name,...arg){
 		for(let m in this.modules)
 			this.modules[m][name]&&this.modules[m][name](...arg);
-	}
-	draw(){
-		if(this.working===false)return;
-		if(this.fps===0){
-			requestAnimationFrame(()=>{this.draw();});
-		}else{
-			setTimeout(()=>{this.draw();},1000/this.fps);
-		}
-		this.moduleFunction('draw');
 	}
 	setMedia(media){
 		this.media=media;
